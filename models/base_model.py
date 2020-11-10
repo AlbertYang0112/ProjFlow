Fix the bug in F1 calculation.

models/base_model.py
@@ -36,7 +36,7 @@ def build_cls_model(inputs, n_his, Ks, Kt, blocks, keep_prob, cls):
     single_pred = y[:, 0, :, :]
     single_pred = tf.math.argmax(single_pred, 2)
     tf.add_to_collection(name='y_pred', value=single_pred)
-    return train_loss, single_pred
+    return train_loss, single_pred, label
 
 
 def build_model(inputs, n_his, Ks, Kt, blocks, keep_prob):

models/trainer.py
@@ -9,6 +9,7 @@ from data_loader.data_utils import gen_batch
 from models.tester import model_inference, model_inference_cls
 from models.base_model import build_model, build_cls_model, model_save
 from os.path import join as pjoin
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 
 import tensorflow as tf
 import numpy as np
@@ -28,7 +29,7 @@ def model_train_cls(inputs, blocks, args, sum_path='./output/tensorboard'):
     keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
     # Define model loss
-    train_loss, pred = build_cls_model(x, n_his, Ks, Kt, blocks, keep_prob, args.cls)
+    train_loss, pred, label = build_cls_model(x, n_his, Ks, Kt, blocks, keep_prob, args.cls)
     tf.summary.scalar('train_loss', train_loss)
 
     # Learning rate settings
@@ -63,13 +64,18 @@ def model_train_cls(inputs, blocks, args, sum_path='./output/tensorboard'):
             start_time = time.time()
             for j, x_batch in enumerate(
                     gen_batch(inputs.get_data('train'), batch_size, dynamic_batch=True, shuffle=True)):
-                summary, _ = sess.run([merged, train_op], feed_dict={x: x_batch[:, 0:n_his + 1, :, :], keep_prob: 1.0})
+                summary, _, prediction, gt = sess.run([merged, train_op, pred, label], feed_dict={x: x_batch[:, 0:n_his + 1, :, :], keep_prob: 1.0})
+                gt = gt[:, 0, :].reshape(-1)
+                prediction = prediction.reshape(-1)
                 writer.add_summary(summary, i * epoch_step + j)
                 if j % 50 == 0:
                     loss_value = \
                         sess.run(train_loss,
                                  feed_dict={x: x_batch[:, 0:n_his + 1, :, :], keep_prob: 1.0})
                     print(f'Epoch {i:2d}, Step {j:3d}: loss {loss_value:.3f}')
+                    trainPrecision, trainRecall, trainF1, _ = precision_recall_fscore_support(gt, prediction, average='macro')
+                    trainAcc = accuracy_score(gt, prediction)
+                    print(f"Acc: {trainAcc:.3%} F1: {trainF1:.3%} Precision: {trainPrecision:.3%} Recall: {trainRecall:.3%}") 
             print(f'Epoch {i:2d} Training Time {time.time() - start_time:.3f}s')
 
             start_time = time.time()

utils/math_utils.py
@@ -7,7 +7,7 @@
 
 import numpy as np
 from numpy.lib.function_base import average
-from sklearn.metrics import f1_score, precision_score, recall_score
+from sklearn.metrics import precision_recall_fscore_support
 
 
 def z_score(x, mean, std):
@@ -74,9 +74,8 @@ def class_evaluation(y, y_):
     tmp2 = gt - np.zeros((sh[0], sh[1]), dtype=int)
     acc = 1 - np.count_nonzero(tmp) / (sh[0] * sh[1])
     acc2 = 1 - np.count_nonzero(tmp2) / (sh[0] * sh[1])  # accuracy if prediction is all zero
-    precision = precision_score(gt, y_, average='macro')
-    recall = recall_score(gt, y_, average='macro')
-    return acc, acc2, F1(gt, y_), precision, recall
+    precision, recall, f1, _ = precision_recall_fscore_support(gt.reshape(-1), y_.reshape(-1), average='macro')
+    return acc, acc2, f1, precision, recall
 
 
 def evaluation(y, y_, x_stats):